f1d122d1cf54864ab0d4fd3bf86ec94bd3d4fe63
Ernesto <ernestoaguaysol@gmail.com>
2018-02-06 21:10:39 -0300

    login component and css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes, RouterModule } from '@angular/router';
+import { PagesComponent } from './pages/pages.component';
 import { NgModule } from '@angular/core';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
@@ -7,12 +8,19 @@ import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 const routes: Routes = [
-    { path: 'dashboard', component: DashboardComponent },
+    { 
+        path: '', 
+        component: PagesComponent,
+        children: [
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'progress', component: ProgressComponent },
+            { path: 'graficas1', component: Graficas1Component },
+            { path: '', pathMatch: 'full' , redirectTo: '/dashboard' },
+        ]
+    },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: LoginComponent },
-    { path: 'progress', component: ProgressComponent },
-    { path: 'graficas1', component: Graficas1Component },
-    { path: '', pathMatch: 'full' , redirectTo: '/dashboard' },
+   
     { path: '**', component: NopagefoundComponent },
 ];
 
