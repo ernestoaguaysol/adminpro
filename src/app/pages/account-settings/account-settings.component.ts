ed219fb298d442e59cbb499a337f5453257e41b3
Ernesto <ernestoaguaysol@gmail.com>
2018-02-20 20:38:56 -0300

    selector and working in classList

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -13,10 +13,22 @@ export class AccountSettingsComponent implements OnInit {
   ngOnInit() {
   }
 
-  cambiarColor(tema: string ) {
-    console.log(tema);
+  cambiarColor(tema: string, link: any ) {
+    console.log(link);
+    
+    this.aplicarCheck(link);
     let url = `assets/css/colors/${tema}.css`;
     this._document.getElementById('tema').setAttribute('href', url );
+  }
+
+  aplicarCheck( link: any ) {
+    let selectores: any = document.getElementsByClassName('selector');
+
+    for ( let ref of selectores) {
+      ref.classList.remove('working');
+    }
+
+    link.classList.add('working');
   }
 
 }
