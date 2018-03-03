8ae31cb2a405409d0194c276352bee80c35d8f9b
Ernesto <ernestoaguaysol@gmail.com>
2018-03-02 21:47:54 -0300

    login normal

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { UsuarioService } from '../services/service.index';
+import { Usuario } from '../models/usuario';
 
 declare function init_plugins();
 
@@ -11,15 +14,29 @@ declare function init_plugins();
 })
 export class LoginComponent implements OnInit {
 
-  constructor( public router: Router) { }
+  recuerdame: boolean = false;
+
+  constructor( 
+      public router: Router,
+      public _usuarioService: UsuarioService) { }
 
   ngOnInit() {
     init_plugins();
   }
 
-  ingresar() {
-    console.log('ingresando');
-    this.router.navigate(['/dashboard']);
+  ingresar(forma: NgForm) {
+    if (forma.invalid) {
+      return;
+    }
+
+    let usuario = new Usuario(null, forma.value.email, forma.value.password);
+
+    this._usuarioService.login(usuario, forma.value.recuerdame)
+        .subscribe( resp => {
+          console.log(resp);
+          
+        });    
+    // this.router.navigate(['/dashboard']);
   }
 
 }
--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -10,6 +10,13 @@ export class UsuarioService {
 
   constructor(public http: HttpClient) { }
 
+  login( usuario: Usuario, recordar: boolean = false) {
+
+    let url = URL_SERVICIOS + '/login';
+
+    return this.http.post(url, usuario);
+  }
+
   crearUsuario(usuario: Usuario) {
 
     let url = `${URL_SERVICIOS}/usuario`;
