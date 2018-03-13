5f06a7d54e5e7cc06b8f25b291f8b0c1f9bb5bb6
Ernesto <ernestoaguaysol@gmail.com>
2018-03-12 21:50:11 -0300

    crud medicos terminado

--- src/app/services/medico/medico.service.ts
+++ src/app/services/medico/medico.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { URL_SERVICIOS } from '../../config/config';
 import { UsuarioService } from '../usuario/usuario.service';
+import { Medico } from '../../models/medico';
 
 @Injectable()
 export class MedicoService {
@@ -40,6 +41,38 @@ export class MedicoService {
                   swal('Médico borrado', 'Médico borrado correctamente', 'success');
                   return resp;
                 });
+  }
+
+  guardarMedico(medico: Medico) {
+    let url = URL_SERVICIOS + '/medico';
+    if (medico._id) {
+      // actualizando
+      url += '/' + medico._id + '?token=' + this._usuarioService.token;
+
+      return this.http.put(url, medico)
+                  .map( (resp: any) => {
+                    swal('Médico actualizado', medico.nombre, 'success');
+                    return resp.medico;
+                  });
+    } else {
+      // creando
+      url += '?token=' + this._usuarioService.token;
+  
+      return this.http.post(url, medico)
+            .map((resp: any) => {
+  
+              swal('Médico creado', medico.nombre, 'success');
+              return resp.medico;
+            });
+    }
+    
+  }
+
+  cargarMedico(id: string) {
+    let url = URL_SERVICIOS + '/medico/' + id;
+
+    return this.http.get(url)
+                .map((resp: any) => resp.medico);
   }
 
 }
