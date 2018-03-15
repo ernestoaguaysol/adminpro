24715c8b8e793d337e7519131e425f52f6ab7891
Ernesto <ernestoaguaysol@gmail.com>
2018-03-15 15:11:01 -0300

    admin guard

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -6,7 +6,11 @@ import { Graficas1Component } from './graficas1/graficas1.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
-import { LoginGuardGuard } from '../services/service.index';
+
+// Guards
+import { LoginGuardGuard, AdminGuard } from '../services/service.index';
+
+
 import { ProfileComponent } from './profile/profile.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
 import { HospitalesComponent } from './hospitales/hospitales.component';
@@ -29,7 +33,11 @@ const pagesRoutes: Routes = [
             { path: 'perfil', component: ProfileComponent , data: { titulo: 'Perfil de Usuario'}},
             { path: 'busqueda/:termino', component: BusquedaComponent , data: { titulo: 'Buscador'}},
             // Mantenimientos
-            { path: 'usuarios', component: UsuariosComponent , data: { titulo: 'Mantenimiento de Usuarios'}},
+            { 
+                path: 'usuarios', 
+                component: UsuariosComponent ,
+                canActivate: [AdminGuard], 
+                data: { titulo: 'Mantenimiento de Usuarios'}},
             { path: 'hospitales', component: HospitalesComponent , data: { titulo: 'Mantenimiento de Hospitales'}},
             { path: 'medicos', component: MedicosComponent , data: { titulo: 'Mantenimiento de Medicos'}},
             { path: 'medico/:id', component: MedicoComponent , data: { titulo: 'Actualizar Medico'}},
--- src/app/services/service.module.ts
+++ src/app/services/service.module.ts
@@ -10,6 +10,7 @@ import {
   SidebarService,
   UsuarioService,
   LoginGuardGuard,
+  AdminGuard,
   SubirArchivoService,
   HospitalService,
   MedicoService
@@ -26,6 +27,7 @@ import {
     SidebarService,
     UsuarioService,
     LoginGuardGuard,
+    AdminGuard,
     SubirArchivoService,
     ModalUploadService,
     HospitalService,
